c274bb7893b5203e2dc580e3588847d35c4b8bd3
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-02-13 20:44:24 +0100

    add files

--- dist/formulari.js
+++ dist/formulari.js
@@ -2,10 +2,9 @@
 // Variables
 const d = document;
 const index = "index.html";
+// Buttons principals
 let goToIndexButton = d.getElementById("button");
 let sendToIndexButton = d.getElementById("Enviar");
-let nomComplet = d.getElementById("nom").value;
-let naixementData = d.getElementById("dataNaixement");
 // Event Listeners
 goToIndexButton.addEventListener('click', goToIndex);
 sendToIndexButton.addEventListener('submit', sendToIndex);
@@ -17,6 +16,24 @@ function sendToIndex(e) {
     e.preventDefault();
     alert("XD");
 }
+// ===============
+//  LOCAL STORAGE
+// ===============
+const returnValue = function (value) {
+    return (d.getElementById(value).value);
+};
+let nomComplet = returnValue("nomComplet");
+let email = returnValue("email");
+let passwd = returnValue("contrasenya");
+let naixementData = d.getElementById("dataNaixement");
+let pelicula = d.getElementById("pellicula");
+let genere = d.getElementById("genere");
 localStorage.setItem("nom", nomComplet);
+localStorage.setItem("email", email);
+localStorage.setItem("passwd", passwd);
 localStorage.setItem("data", JSON.stringify(naixementData));
+localStorage.setItem("pelis", JSON.stringify(pelicula));
+localStorage.setItem("genere", JSON.stringify(genere));
+// No se si funcionara pero hago un alert pa enterarme de algo.
+// alert(localStorage);
 //# sourceMappingURL=formulari.js.map
--- formulari.ts
+++ formulari.ts
@@ -2,10 +2,9 @@
 const d: Document = document;
 const index: string = "index.html";
 
+// Buttons principals
 let goToIndexButton: HTMLButtonElement = d.getElementById("button") as HTMLButtonElement;
 let sendToIndexButton: HTMLInputElement = d.getElementById("Enviar") as HTMLInputElement;
-let nomComplet : string = (d.getElementById("nom")as HTMLInputElement).value;
-let naixementData : HTMLSpanElement = d.getElementById("dataNaixement") as HTMLSpanElement;
 
 // Event Listeners
 goToIndexButton.addEventListener('click', goToIndex);
@@ -21,5 +20,25 @@ function sendToIndex(e: Event) {
     alert("XD");
 }
 
-localStorage.setItem("nom",  nomComplet);
-localStorage.setItem("data",  JSON.stringify(naixementData));
+// ===============
+//  LOCAL STORAGE
+// ===============
+const returnValue = function (value: string): string {
+    return ((d.getElementById(value) as HTMLInputElement).value);
+}
+
+let nomComplet: string = returnValue("nomComplet");
+let email: string = returnValue("email");
+let passwd: string = returnValue("contrasenya");
+let naixementData: HTMLSpanElement = d.getElementById("dataNaixement") as HTMLSpanElement;
+let pelicula: HTMLDataListElement = d.getElementById("pellicula") as HTMLDataListElement;
+let genere: HTMLSelectElement = d.getElementById("genere") as HTMLSelectElement;
+
+localStorage.setItem("nom", nomComplet);
+localStorage.setItem("email", email);
+localStorage.setItem("passwd", passwd);
+localStorage.setItem("data", JSON.stringify(naixementData));
+localStorage.setItem("pelis", JSON.stringify(pelicula));
+localStorage.setItem("genere", JSON.stringify(genere));
+// No se si funcionara pero hago un alert pa enterarme de algo.
+// alert(localStorage);
